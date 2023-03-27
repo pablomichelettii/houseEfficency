06084de6479ded06e3866703e0e2557bbd3d9663
Daniel Angelozzi <d.angelozzi@hastega.it>
2023-03-27 12:32:43 +0200

    Add Script to generate all

---
+++ generate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "alfa"
+python3 solution.py inputs/alfa.txt outputs/alfa.txt
+
+echo "beta"
+python3 solution.py inputs/beta.txt outputs/beta.txt
+
+echo "gamma"
+python3 solution.py inputs/gamma.txt outputs/gamma.txt
+
+echo "delta"
+python3 solution.py inputs/delta.txt outputs/delta.txt
